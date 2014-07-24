c5bc7a8996eafa33d65a03242ff50528e5cc703f
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-07-24 19:56:43 -0300

    Terminando a classe de Clientes, adicionando máscaras nos campos

--- class/Cliente.class.php
+++ class/Cliente.class.php
@@ -1,12 +1,38 @@
 <?php
+    require_once("functions/mask.php");
+
     class Cliente {
         public $nome;
         public $cpf;
         public $endereco;
+        public $cidade;
+        public $telefone;
 
-        public function __construct($nome, $cpf, $endereco) {
+        public function __construct($nome, $cpf, $endereco, $cidade, $telefone) {
             $this->nome = $nome;
-            $this->cpf = $cpf;
+            $this->cpf = $this->formatarValor($cpf)->cpf();
             $this->endereco = $endereco;
+            $this->cidade = $cidade;
+            $this->telefone = $this->formatarValor($telefone)->telefone();
+        }
+
+        private function formatarValor($valor) {
+            $this->valor_tmp = $valor;
+            return $this;
+        }
+
+        private function telefone() {
+            $mask = (strlen($this->valor_tmp) == 10 ? "(##) ####-####" : "(##) # ####-####");
+            $resultado = mask($this->valor_tmp, $mask);
+            unset($this->valor_tmp);
+
+            return $resultado;
+        }
+
+        private function cpf() {
+            $cpf = $this->valor_tmp;
+            unset($this->valor_tmp);
+
+            return mask($cpf, "###.###.###-##");
         }
     }
