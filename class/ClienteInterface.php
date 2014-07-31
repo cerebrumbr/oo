0d0e89b0a8d58efa8bf9463645013ca70ff1811a
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-07-31 00:15:52 -0300

    Adicionando todos os setters e getters na interface

--- class/ClienteInterface.php
+++ class/ClienteInterface.php
@@ -1,6 +1,14 @@
 <?php
 
     interface ClienteInterface {
+        public function setNome();
+        public function getNome();
+        public function setCpf();
+        public function getCpf();
+        public function setEndereco();
+        public function getEndereco();
+        public function setTelefone();
+        public function getTelefone();
         public function setGrauImportancia($estrelas = 1);
         public function getGrauImportancia();
     }
